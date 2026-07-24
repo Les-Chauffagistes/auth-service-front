b52a766f882b99435e754d6a03a8e8d77497e646
Hugo Beets <hugo.beets@fundsdlt.net>
2026-07-24 10:56:02 +0200

    added back button to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,13 @@ import styles from "./page.module.css";
 import { components } from "@les-chauffagistes/authentication-types";
 import { getMe, logOut } from "./api";
 import LinkWithSearchParams from "./components/LinkWithSearchParams";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   const [user, setUser] = useState<components["schemas"]["User"] | undefined | null>(undefined);
+  const searchParams = useSearchParams();
+  const redirect = searchParams.get("redirect");
+  const appName = searchParams.get("appname");
 
   useEffect(() => {
     getMe().then(setUser);
@@ -16,6 +20,16 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.main}>
+        {redirect && (
+            <button
+                className={"tertiary"}
+                onClick={() => {
+                  window.location.href = redirect;
+                }}
+            >
+                {appName ? `Retourner sur ${appName}` : "Retourner là où j'étais"}
+            </button>
+        )}
         {user === undefined && <p>Loading...</p>}
         {user === null && <>
             <p>Pas connecté</p>
